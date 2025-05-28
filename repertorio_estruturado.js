1421dce84452f05dcb2fb2aa7a772dd47762118e
AgoraeuSaquei <christianobzs@gmail.com>
2025-05-28 00:14:10 -0300

    Add files via upload

---
+++ repertorio_estruturado.js
@@ -0,0 +1,242 @@
+const repertorio = [
+  {
+    artista: "Alma Negra",
+    musicas: ["Filosofia Rasta"]
+  },
+  {
+    artista: "Maneva",
+    musicas: ["Pisando descalço"]
+  },
+  {
+    artista: "Charlie Brown Jr",
+    musicas: [
+      "Senhor do tempo",
+      "Ela vai voltar",
+      "Tudo que ela gosta de escutar",
+      "Dias de Luta Dias de Glória",
+      "O preço",
+      "Como tudo deve ser",
+      "Zóio di Lula",
+      "Longe de você",
+      "Céu Azul",
+      "Quinta-feira",
+      "Só por uma noite",
+      "Não é sério"
+    ]
+  },
+  {
+    artista: "O Rappa",
+    musicas: [
+      "Meu mundo é um barro",
+      "Óbvio",
+      "Súplica cearense",
+      "Minha alma",
+      "Vapor barato",
+      "Reza Vela",
+      "Mar de Gente",
+      "Pescador de ilusões",
+      "Papo de surdo e mudo",
+      "A feira",
+      "Hey Joe",
+      "Hóstia",
+      "7 vezes",
+      "Farpa cortante",
+      "My brother",
+      "Verdade de feirante",
+      "Não perca as crianças de vista",
+      "O salto",
+      "Maria",
+      "Tribunal de rua",
+      "Meu Santo tá cansado",
+      "Miséria S.A."
+    ]
+  },
+  {
+    artista: "Natiruts",
+    musicas: [
+      "Naticongo",
+      "Eu quero ser feliz também",
+      "Andei só",
+      "Liberdade pra dentro da cabeça",
+      "Você me encantou demais",
+      "Supernova",
+      "Pedras escondidas",
+      "Dentro da música"
+    ]
+  },
+  {
+    artista: "Skank",
+    musicas: [
+      "Jack tequila",
+      "Acima do Sol",
+      "O marujo e a vela"
+    ]
+  },
+  {
+    artista: "Leoni",
+    musicas: ["Garotos II"]
+  },
+  {
+    artista: "Seu Jorge",
+    musicas: [
+      "Burguesinha",
+      "Teu olhar",
+      "Tive razão"
+    ]
+  },
+  {
+    artista: "Tim Maia",
+    musicas: [
+      "É primavera",
+      "Não sei porque você se foi",
+      "Do Leme ao Pontal",
+      "Ela partiu"
+    ]
+  },
+  {
+    artista: "Cidade Negra",
+    musicas: [
+      "Filhos feitos de amor",
+      "O erê",
+      "Girassol",
+      "Podes crer",
+      "A estrada"
+    ]
+  },
+  {
+    artista: "Paulinho Moska",
+    musicas: ["Pensando em você"]
+  },
+  {
+    artista: "Armandinho",
+    musicas: [
+      "Eu juro",
+      "Ursinho de dormir",
+      "Desenho de deus",
+      "Paulinha",
+      "Pescador",
+      "Lua cheia",
+      "Ana lua",
+      "Folha de bananeira"
+    ]
+  },
+  {
+    artista: "Djavan",
+    musicas: [
+      "Boa noite",
+      "Te devoro",
+      "Oceano",
+      "Sina"
+    ]
+  },
+  {
+    artista: "Jorge Vercilo",
+    musicas: [
+      "Meu amor",
+      "Homem aranha"
+    ]
+  },
+  {
+    artista: "Zeca Baleiro",
+    musicas: [
+      "Telegrama",
+      "A flor da pele"
+    ]
+  },
+  {
+    artista: "Falamansa",
+    musicas: [
+      "Oh chuva",
+      "Medo do escuro",
+      "Avisa",
+      "Xote universitário",
+      "Xote dos milagres",
+      "Xote das meninas",
+      "Xote da alegria"
+    ]
+  },
+  {
+    artista: "Vanessa Rangeu",
+    musicas: ["Alpiste"]
+  },
+  {
+    artista: "Cassia Eller",
+    musicas: [
+      "Segundo sol",
+      "Nós",
+      "Laranjeiras",
+      "Luz dos olhos",
+      "E.C.T.",
+      "Palavras"
+    ]
+  },
+  {
+    artista: "Nando Reis",
+    musicas: [
+      "Relicário",
+      "Cegos do castelo"
+    ]
+  },
+  {
+    artista: "Caetano Veloso",
+    musicas: [
+      "Leãozinho",
+      "Tempo"
+    ]
+  },
+  {
+    artista: "Maria Gadu",
+    musicas: ["Laranja"]
+  },
+  {
+    artista: "Legião Urbana",
+    musicas: [
+      "Tempo perdido",
+      "Eduardo e mônica",
+      "Ainda e cedo",
+      "Que pais é esse"
+    ]
+  },
+  {
+    artista: "Cazuza/Barão Vermelho",
+    musicas: [
+      "Ideologia",
+      "Bete balanço",
+      "O poeta está vivo",
+      "O tempo não para",
+      "Por você"
+    ]
+  },
+  {
+    artista: "Autorais/amigos",
+    musicas: [
+      "Zueira",
+      "Por mais que o esteja errado",
+      "Regue o amor",
+      "Búzios 40°",
+      "Me diz",
+      "Alguém mais forte",
+      "Conto do amanhecer",
+      "Nem sempre é a solução",
+      "Todos nós",
+      "O sábio",
+      "Aqui jazz meu coração",
+      "Everybody come on",
+      "Girassol",
+      "Entre o sol e a lua",
+      "Nostalgia",
+      "Orvalhos",
+      "Eu vi",
+      "Inocência"
+    ]
+  },
+  {
+    artista: "Alceu Valença",
+    musicas: [
+      "La Belle du jeur",
+      "Marisol",
+      "Anunciação",
+      "Frevo mulher"
+    ]
+  }
+];
